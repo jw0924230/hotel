bf61f98c884e7836a9bfabee25372c289612fcc9
Kevin <kevin@Kevins-MacBook-Air.local>
2026-07-10 22:48:28 +0800

    chore: remove unused hotel detail JSON files and associated images

--- app/app.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTR8MG73"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
-</template>
